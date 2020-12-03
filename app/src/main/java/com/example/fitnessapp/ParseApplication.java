a46a62a65c760cc797a7a46db43b698e96ce726b
arsalaerfan <erfanarsala831@gmail.com>
2020-12-03 10:39:13 -0800

    Implemented the Sign up activity and connected it to Parse

--- app/src/main/java/com/example/fitnessapp/ParseApplication.java
+++ app/src/main/java/com/example/fitnessapp/ParseApplication.java
@@ -31,18 +31,12 @@ public class ParseApplication extends Application {
         ParseObject.registerSubclass(Exercise.class);
 
 
-//        Parse.initialize(new Parse.Configuration.Builder(this)
-//                .applicationId(APP_API_KEY)
-//                .clientKey(ClIENT_KEY)
-//                .server("https://parseapi.back4app.com")
-//                .build()
-//        );
-
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("oKMOelDcUXdM2M4ONnhJL9fKUHQnSSr6dy5cYxnK")
-                .clientKey("g6M0dfasOiplXHh63Mr2W5u1Lh4YHyZ8nHCgXoT1")
+                .applicationId(APP_API_KEY)
+                .clientKey(ClIENT_KEY)
                 .server("https://parseapi.back4app.com")
                 .build()
         );
+
     }
 }
